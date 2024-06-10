cb0cf8de3536d49baaabe822d206ef83cc778e1a
yuna-hh <ghddbsk12@naver.com>
2024-06-10 21:29:13 +0900

    🩹 Props drilling 수정
    Router에서 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,29 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import Detail from "../pages/Detail";
-import { useState } from "react";
 
 const Router = () => {
-  const [cashArray, setCashArray] = useState([]);
-  const [clickMonth, setClickMonth] = useState(1);
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <HomePage
-              cashArray={cashArray}
-              setCashArray={setCashArray}
-              clickMonth={clickMonth}
-              setClickMonth={setClickMonth}
-            />
-          }
-        ></Route>
-        <Route
-          path="/detail/:detailId"
-          element={<Detail cashArray={cashArray} setCashArray={setCashArray} />}
-        ></Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/detail/:detailId" element={<Detail />}></Route>
       </Routes>
     </BrowserRouter>
   );
